d462bcbc9e181b37d3cc4cdb3cabcdb1be4ae60f
ralfcam <joser.campost@gmail.com>
2020-03-24 16:59:23 +0100

    V.2 New Look 2020

---
+++ whatsapp_btn.js
@@ -0,0 +1,6 @@
+document.getElementById('whatsappcontact').onmouseover = function(){
+document.getElementById('whtmessagetext').style.display = 'inline-block';
+}
+document.getElementById('whatsappcontact').onmouseout = function(){
+document.getElementById('whtmessagetext').style.display = 'none';
+}
